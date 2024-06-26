cc79f8775403943b619c4f5893f9e793425b1f4f
Kevinsaez <kevinsaez10@gmail.com>
2024-06-26 04:36:49 -0300

    animacion estrellas

---
+++ script.js
@@ -0,0 +1,25 @@
+const colors = ["#fff2", "#fff4", "#fff7", "#fffc"];
+const generateSpaceLayer = (size, selector, totalStars, duration) => {
+
+    const layer = [];
+
+
+    for (let i = 0; i < totalStars; i++) {
+
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        const x = Math.floor(Math.random() * 100);
+        const y = Math.floor(Math.random() * 100);
+        layer.push(`${x}vw ${y}vh 0 ${color} ,  ${x}vw ${y + 100}vh 0 ${color} `);
+
+    }
+
+    const container = document.querySelector(selector);
+    container.style.setProperty("--space-layer", layer.join(","));
+    container.style.setProperty("--size", size);
+    container.style.setProperty("--duration", duration);
+
+}
+
+generateSpaceLayer("2px", ".space-1", 200, "25s");
+generateSpaceLayer("3px", ".space-2", 100, "20s");
+generateSpaceLayer("6px", ".space-3", 25, "15s");
